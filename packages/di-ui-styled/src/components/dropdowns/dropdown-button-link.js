abef39bd25ebb1238a58a70bf89186fd41e0176c
Phil Holden <phil.m.holden@gmail.com>
2017-03-10 16:06:16 +0000

    0.2.11

---
+++ packages/di-ui-styled/src/components/dropdowns/dropdown-button-link.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+import Caret from '../caret'
+import Spacer from '../spacer'
+import { fromTheme } from '../../utils/theme-util'
+
+const Wrapper = styled.div`
+  box-sizing: border-box;
+  user-select: none;
+  cursor: default;
+  text-align: right;
+  white-space: nowrap;
+  display: inline-block;
+  width: 100%;
+`
+
+const Ellipsis = styled.span`
+  flex: 1;
+  padding: 0 16px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`
+
+const CaretWrapper = styled.div`
+  width: 37px;
+`
+
+const Link = styled.span`
+  color: ${fromTheme('sectionCColor')};
+  text-decoration: underline;
+  font-size: 14px;
+`
+
+const DropdownButtonLink = ({ children, ...props }) => (
+  <Wrapper {...props} tabIndex="0">
+    <Link>{children}</Link>
+    <Spacer />
+    <Caret />
+  </Wrapper>
+)
+
+export default DropdownButtonLink
---
+++ packages/di-ui-styled/src/components/dropdowns/dropdown-link.js
@@ -0,0 +1,56 @@
+// @flow
+
+import React, { Component } from 'react'
+import DropdownList from './dropdown-list'
+import DropdownButton from './dropdown-button'
+import DropdownButtonLink from './dropdown-button-link'
+
+type OptionType = {
+  label: string,
+}
+
+type DropdownPropsType = {
+  /**
+    * Options are objects which must have a label key.
+    * They can have any other keys which may be used
+    * by custom filters functions or onChange handlers.
+    */
+  options: Array<{ label: string }>,
+  /*
+   * make button white on gray background
+   */
+  onGray: boolean,
+  /*
+   * multiselect dropdown
+   */
+  multiple: boolean,
+  width: number,
+  noWrap: boolean,
+  /*
+   * show filter. defaults 'space' meaning 'and' and quotes meaning exactly
+   */
+  filter: boolean,
+  /*
+   * override filter function.
+   */
+  filterFn: (filterText: string) => (option: {
+    label: string,
+  }) => boolean,
+  /*
+   * array when multiple true
+   */
+  value: OptionType,
+  /*
+   * array when multiple true
+   */
+  defaultValue: OptionType,
+  /*
+   * array when multiple true
+   */
+  onChange: (value: OptionType | Array<OptionType>) => void,
+}
+
+const DropdownLink = (props: DropdownPropsType) =>
+  <DropdownList {...props} button={DropdownButtonLink} />
+
+export default DropdownLink
